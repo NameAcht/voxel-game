230ab1012cf107789bc226591dc4b58492e69b32
NameAcht <appeljohannes21@gmail.com>
2024-02-23 23:13:49 +0100

    basic chunking and draw optimization, currently unsafe

---
+++ include/block.h
@@ -0,0 +1,2 @@
+#pragma once
+#include <cglm/struct.h>
--- include/chunk.h
+++ include/chunk.h
@@ -1,6 +1,23 @@
 #pragma once
 #include <cglm/struct.h>
+#include <block.h>
+
+#define CHUNK_SIZE 4096
+
+// store Chunk position and block ID
 typedef struct {
-	float pos;
-	vec3s* blocks;
+	unsigned short blocks[16][16][16];
+	ivec3s pos;
 } Chunk;
+
+// Directions
+static ivec3 DIR_VECS[] = {
+	{  0,  0, -1  },
+	{  0,  0,  1  },
+	{ -1,  0,  0  },
+	{  1,  0,  0  },
+	{  0, -1,  0  },
+	{  0,  1,  0  }
+};
+
+bool adjBlock(Chunk* c);
